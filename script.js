f2fec82320ea5532c9ea983bd9e9a3f5f36ff9f0
v8ive <62786822+v8ive@users.noreply.github.com>
2025-09-10 20:49:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
+import { social_links, albums } from '/config.js';
+
 document.addEventListener('DOMContentLoaded', () => {
-    const configScript = document.createElement('script');
-    configScript.src = `/config.js?v=${new Date().getTime()}`;
-    
-    configScript.onload = () => {
-        initializeApp();
-    };
-    
-    document.head.appendChild(configScript);
+    initializeApp();
 });
 
 function initializeApp() {
@@ -20,7 +15,7 @@ function initializeApp() {
 
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/sw.js')
+            navigator.serviceWorker.register('/service-worker-v2.js')
                 .then(registration => {
                     console.log('ServiceWorker registration successful with scope: ', registration.scope);
                 })
